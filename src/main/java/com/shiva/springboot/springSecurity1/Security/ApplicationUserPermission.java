8aa4ccc0d386cb20bad0286fe53232df7d27210a
Brahma <brahmananda647@gmail.com>
2022-04-05 23:33:13 +0530

    permissions

---
+++ src/main/java/com/shiva/springboot/springSecurity1/Security/ApplicationUserPermission.java
@@ -0,0 +1,22 @@
+package com.shiva.springboot.springSecurity1.Security;
+
+public enum ApplicationUserPermission {
+
+    STUDENT_READ("student:read"),
+    STUDENT_WRITE("student:write"),
+    COURSE_READ("course:read"),
+    COURSE_WRITE("course:write");
+
+    private String permissions;
+
+
+
+     ApplicationUserPermission(String permissions){
+
+        this.permissions = permissions;
+    }
+
+
+
+
+}
