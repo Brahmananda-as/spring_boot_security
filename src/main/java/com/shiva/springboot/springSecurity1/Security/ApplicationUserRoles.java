f6fe2cfb60dd4fcfa868dad193249ca1560856cc
Brahma <brahmananda647@gmail.com>
2022-04-06 10:14:59 +0530

    coded

--- src/main/java/com/shiva/springboot/springSecurity1/Security/ApplicationUserRoles.java
+++ src/main/java/com/shiva/springboot/springSecurity1/Security/ApplicationUserRoles.java
@@ -1,4 +1,27 @@
 package com.shiva.springboot.springSecurity1.Security;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.shiva.springboot.springSecurity1.Security.ApplicationUserPermission.*;
+
 public enum ApplicationUserRoles {
+
+    ADMIN(Sets.newHashSet(STUDENT_READ,STUDENT_WRITE,COURSE_WRITE,COURSE_READ)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ,STUDENT_READ)),
+    STUDENT(Sets.newHashSet(STUDENT_READ));
+
+
+
+    private  final Set<ApplicationUserPermission> permissions;
+
+
+    ApplicationUserRoles(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
 }
