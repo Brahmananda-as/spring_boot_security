8b49fac0fe0ef5aaf56d839d0d9f8d873220974e
Brahma <brahmananda647@gmail.com>
2022-04-07 22:30:07 +0530

    http methods added

--- src/main/java/com/shiva/springboot/springSecurity1/Security/ApplicationSecurityconfig.java
+++ src/main/java/com/shiva/springboot/springSecurity1/Security/ApplicationSecurityconfig.java
@@ -1,8 +1,10 @@
 package com.shiva.springboot.springSecurity1.Security;
 
+import com.shiva.springboot.springSecurity1.student.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.shiva.springboot.springSecurity1.Security.ApplicationUserPermission.COURSE_WRITE;
 import static com.shiva.springboot.springSecurity1.Security.ApplicationUserRoles.*;
 
 @Configuration
@@ -33,11 +36,16 @@ public class ApplicationSecurityconfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
+                .csrf().disable()
                 .authorizeHttpRequests()
                 .antMatchers("/","index")
                 .permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-                .antMatchers("/management/**").hasRole(ADMIN.name())
+//                .antMatchers("/management/**").hasRole(ADMIN.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermissions())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermissions())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermissions())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name(), STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -51,19 +59,22 @@ public class ApplicationSecurityconfig extends WebSecurityConfigurerAdapter {
         UserDetails shiva = User.builder()
                 .username("shiva")
                 .password(passwordEncoder.encode("test"))
-                .roles(STUDENT.name())
+//                .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails parvathi = User.builder()
                 .username("parvathi")
                 .password(passwordEncoder.encode("test"))
-                .roles(ADMIN.name())
+              //  .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails gayathri = User.builder()
                 .username("gayathri")
                 .password(passwordEncoder.encode("test"))
-                .roles(ADMINTRAINEE.name())
+//                .roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new  InMemoryUserDetailsManager(shiva,parvathi,gayathri);
--- src/main/java/com/shiva/springboot/springSecurity1/Security/ApplicationUserRoles.java
+++ src/main/java/com/shiva/springboot/springSecurity1/Security/ApplicationUserRoles.java
@@ -1,8 +1,10 @@
 package com.shiva.springboot.springSecurity1.Security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.shiva.springboot.springSecurity1.Security.ApplicationUserPermission.*;
 
@@ -24,4 +26,21 @@ public enum ApplicationUserRoles {
     public Set<ApplicationUserPermission> getPermissions() {
         return permissions;
     }
+
+    public  Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+
+       Set<SimpleGrantedAuthority> authorities =     getPermissions().stream().map(
+                    permissions-> new SimpleGrantedAuthority(permissions.getPermissions())
+
+            ).collect(Collectors.toSet());
+
+        authorities.add(new SimpleGrantedAuthority("ROLE"+this.name()));
+        return  authorities;
+
+    }
+
+
+
+
+
 }
